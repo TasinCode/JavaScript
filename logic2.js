66a5882fa1ed901c441cade80f5803931cde1b09
TasinCode <codewithtasin@gmail.com>
2023-10-31 13:55:58 +0600

    Concatenate strings, Number method, Variables in Javascript

---
+++ logic2.js
@@ -0,0 +1,45 @@
+var num = 20;
+
+num = toString(num);
+
+console.log(typeof(num));
+
+
+
+var a = "20";
+
+a = parseInt(a);
+
+console.log(typeof(a));
+
+
+
+var b = "20.666";
+
+b = parseFloat(b);
+
+console.log(typeof(b));
+
+
+
+var number = 3.35789;
+
+console.log(number.toFixed(4));
+
+
+var number = 4.25489;
+
+console.log(number.toPrecision(4));
+
+
+console.log(Number("65"));
+
+console.log(typeof(Number("14")));
+
+console.log(typeof(Number("14.325")));
+
+
+console.log(Number(true));
+
+
+console.log(Number(false));
