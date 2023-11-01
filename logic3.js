4bbe12b5f431b39c1adb75e2852260df2eaa8932
TasinCode <codewithtasin@gmail.com>
2023-11-02 00:14:29 +0600

    Operator, Library Funtions in JavaScript

---
+++ logic3.js
@@ -0,0 +1,8 @@
+//Arithmetic Operator -> +, -, *, /, %(Modulus), **(Exponent), ++(increment), --(decrement)
+
+//Assigment Operator -> =, +=, -=, *=, /=, %=, **=
+
+var x=8;
+x += 8; //x = x+8
+
+console.log(x);
