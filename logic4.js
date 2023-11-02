ae0b678b679f3e4d1b7a28fff505b3a243a299fe
TasinCode <codewithtasin@gmail.com>
2023-11-02 13:03:47 +0600

    Temp Converter, Area of Various Shape, Calculator in JS

---
+++ logic4.js
@@ -0,0 +1,48 @@
+
+
+/*var num1 = prompt("Enter First Number : ");
+var num2 = prompt("Enter Second Number : ");
+
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10);
+
+
+
+var sum, sub;
+
+sum = num1 + num2;
+document.write("sum = " + sum);
+
+
+sub = num1 - num2;
+document.write(" <br>sub = " + sub);*/
+
+
+var n1 = prompt("Enter first Number : ")
+var n2 = prompt("Enter second Number : ")
+
+n1 = parseInt(n1, 10);
+n2 = parseInt(n2, 10);
+
+var sum, sub, mult, div, mod;
+
+sum = n1 + n2;
+document.write(n1 + " +" + n2 + " = " + sum + "<br>");
+
+
+sub = n1 - n2;
+document.write(n1 + " -" + n2 + " = " + sub + "<br>");
+
+
+mult = n1 * n2;
+document.write(n1 + " *" + n2 + " = " + mult + "<br>");
+
+
+div = n1 / n2;
+document.write(n1 + " /" + n2 + " = " + div + "<br>");
+
+
+mod = n1 % n2;
+document.write(n1 + " %" + n2 + " = " + mod + "<br>");
+
+ 
