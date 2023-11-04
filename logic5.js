3fdf4c2fea95964531c47b8a0c6bea8c1d97f761
TasinCode <codewithtasin@gmail.com>
2023-11-04 11:00:51 +0600

    Control Statement, Relational & Logical Operator in JS

---
+++ logic5.js
@@ -0,0 +1,36 @@
+// Relational Operator -> >, >=, <, <=, ==, ===, !=, !==
+
+// Logical Operator -> &&, ||, !
+
+
+//Control Statement , If, If else, else
+
+
+var num = prompt(" Enter the number : ");
+
+if (num % 2 == 0)
+document.write("<br> Even  ");
+
+else
+document.write(" <br> odd");
+
+
+
+
+var num = prompt("Enter a number : ");
+
+if (num > 0)
+console.log("Positive");
+
+else if (num < 0)
+console.log("Negative");
+
+else
+console.log("Zero");
+
+
+
+
+
+
+
