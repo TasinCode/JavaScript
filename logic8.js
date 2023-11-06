59a327632e43770229e8b4782ae5cb987c56dc63
TasinCode <codewithtasin@gmail.com>
2023-11-06 10:26:09 +0600

    Ternary Operator, Break & Continue in JS

---
+++ logic8.js
@@ -0,0 +1,23 @@
+
+for(i=1; i<=100; i++){
+    if ( i == 10 ){
+        break;
+    }
+
+
+document.write("<br>" + i + "<br>");
+
+}
+
+
+for(i=1; i<=20; i++){
+    if ( i == 10 ){
+        continue;
+    }
+
+
+document.write("" + i);
+
+}
+
+
