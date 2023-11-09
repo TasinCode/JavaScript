793d25771ce7c30eaf2e90594e6f77e07bdb3588
TasinCode <codewithtasin@gmail.com>
2023-11-10 01:16:51 +0600

    Date object & method, Guessing Game by JS

---
+++ logic13.js
@@ -0,0 +1,29 @@
+// // Guessing Game
+// // Guess a number from 1 to 5
+// // Generate a random number between 1 to 5
+// // If the guess number matches random number; then show message won else lost
+// // Run the game for 5 times
+// // Show the number of wons and losts
+
+
+// var numOfWon = 0;
+// var numOfLost = 0;
+
+// for(var i = 1; i<= 5; i++){
+//     var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+//     var randomNumber = Math.floor(Math.random()*5) + 1;
+
+//     if(guessNumber == randomNumber){
+//     console.log("You have won");
+//     numOfWon++;
+//     }
+//     else{
+//     console.log("You have lost. Random number was: " + randomNumber);
+//     numOfLost++;
+//     }
+
+// }
+
+// document.write("Total number of won = " + numOfWon + "<br>");
+
+// document.write("Total number of lost = " + numOfLost + "<br>");
