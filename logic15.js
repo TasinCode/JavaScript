5c25bc97fb6a62f2b3501501c1e4c6736d596008
TasinCode <codewithtasin@gmail.com>
2023-11-11 15:23:34 +0600

    Event handler to onclick event in JS

---
+++ logic15.js
@@ -0,0 +1,33 @@
+function msg1(){
+    alert("Hello World!");
+}
+
+
+
+function msg2(){
+    alert("Hello Everyone!");
+}
+
+var myVar = document.querySelector("#click-me");
+
+
+function msg3(){    
+    myVar.innerHTML = "You have clicked on button-1"
+}
+
+function msg4(){
+    myVar.innerHTML = "You have clicked on button-2"
+}
+
+
+
+var mypVar = document.querySelector("#imgId");
+
+function pic1() {    
+    mypVar.src = "Images\imgforjs1.jpg";
+}
+
+function pic2() {
+    mypVar.src = "Images\imgforjs2.jpg";
+}
+
