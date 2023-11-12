a0cde770ba134a5d9a1160ead5a83f46905d4218
TasinCode <codewithtasin@gmail.com>
2023-11-12 11:19:00 +0600

    DOM traversing & manipulating in JS

--- logic17.js
+++ logic17.js
@@ -16,4 +16,15 @@ const studentList = studentA.parentElement;
 
 //Sideways
 // 1. nextElementSibling, previousElementSibling
+const studentA = document.querySelector("li");
+const studentB = studentA.nextElementSibling;
+const studentC = studentA.nextElementSibling;
+
+
+const studentC = document.querySelectorAll("li")[2];
+const studentB = studentC.previousElementSibling;
+
 // 2. parentElement + children + index
+const studentA = document.querySelector("li");
+const studentList = studentA.parentElement;
+const studentC = studentsList.children[2];
