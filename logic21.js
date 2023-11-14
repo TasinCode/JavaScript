be3026725e73fa7f4d5a647c2114086714625eed
TasinCode <codewithtasin@gmail.com>
2023-11-14 11:02:17 +0600

    Play Audio and Animation in JS

--- logic21.js
+++ logic21.js
@@ -1,37 +1,5 @@
 for (var i=0; i < 3; i++){
 
-document.querySelectorAll(".myButton1")[i].addEventListener("click",function() {
-    var text = this.innerHTML;
-    console.log(text);
-    audioPlay(text);
-
-});
-}
-
-function audioPlay(text){
-    switch(text){
-        case "a":
-            var audio = new Audio("Sounds\a.mp3");
-            audio.play();
-            break;
-
-        case "b":
-            var audio = new Audio("b.mp3");
-            audio.play();
-            break;
-
-        case "c":
-            var audio = new Audio("c.mp3");
-            audio.play();
-            break;
-    }
-}
-
-
-
-
-for (var i=0; i < 3; i++){
-
     document.querySelectorAll(".myButton")[i].addEventListener("click",function() {
         var text = this.innerHTML;
         console.log(text);
@@ -40,6 +8,12 @@ for (var i=0; i < 3; i++){
     
     });
     }
+
+    document.addEventListener("keypress",function(event){
+        var text = event.key;
+        audioPlay(text);
+        playAnimation(text);
+    });
     
     function audioPlay(text){
         switch(text){
@@ -69,3 +43,11 @@ for (var i=0; i < 3; i++){
             selectedButton.classList.remove("anim");
         },1000);
     }
+//keypress listener
+
+var count = 0;
+    document.querySelector("textarea").addEventListener("keypress", function(event){
+        count++;
+        var text = event.key;
+        document.querySelector("p").innerHTML = "You have pressed  " + count;
+    });
