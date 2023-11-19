a68b03d79ed09aa416cc2fa7e0a216e6cf47aa0b
TasinCode <codewithtasin@gmail.com>
2023-11-19 10:42:28 +0600

    BOM Timing events & Clock in JS

--- logic30.js
+++ logic30.js
@@ -41,3 +41,31 @@ function displayCount(){
         message.textContent = count;
     }, 3000);
 }
+
+
+saveButton.addEventListener("click", startClock);
+
+function startClock(){
+    let date    =  new Date();
+    let hours   =  date.getHours();
+    let minutes =  date.getMinutes();
+    let seconds =  date.getSeconds();
+
+    minutes = formatTime(minutes);
+    seconds = formatTime(seconds);
+
+
+    let time =  hours + ":" + minutes + ":" + seconds;
+
+    message.textContent = time;
+
+    setInterval(startClock, 1000);
+
+}
+
+function formatTime(value){
+ if (value < 10){
+    value = "0" + value;
+ }
+ return value;
+}
