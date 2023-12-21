0792d177c8a91b64b7321d425420ed85cc3cf98c
codewithtasin <codewithtasin@gmail.com>
2023-12-21 10:52:34 +0600

    Practice Set-7

---
+++ Practice Set/p-script7.js
@@ -0,0 +1,83 @@
+//  Problem-1
+
+// let age = prompt("What is your age?");
+// age = Number.parseInt(age);
+
+// if(age >= 18){
+//     alert("You can drive!");
+// } else if(age < 18){
+//     alert("You can't drive because you are below 18!");
+// }
+
+//  Another way to solve this (calling function)
+
+// let age1 = prompt("What is your age?");
+// age1 = Number.parseInt(age1);
+
+// const candrive = (age1) => {
+//     return age1 >= 18? true : false;
+// }
+
+// if (candrive (age1)){
+//     alert("You can drive!");
+// }else{
+//     alert("You can't drive because you are below 18!");
+// }
+
+//  Problem-2
+
+
+
+// let runAgain = true;
+
+// while(runAgain){
+
+//     let age = prompt("What is your age?");
+//     age = Number.parseInt(age);
+
+//     if(age >= 18){
+//     alert("You can drive!");
+// } else if(age < 18){
+//     alert("You can't drive because you are below 18!");   
+// }
+// runAgain = confirm("Do you want to enter your age  again?");
+// }
+
+// Problem-3
+
+// let runAgain = true;
+// while(runAgain){
+
+//         let age = prompt("What is your age?");
+//         age = Number.parseInt(age);
+
+//     if(age<0){
+//         console.error("Please enter a valid age");  // using console.error 
+//         break;
+//     }
+
+//     if(age >= 18){
+//         alert("You can drive!");
+//     } else if(age < 18){
+//         alert("You can't drive because you are below 18!");   
+//     }
+//     runAgain = confirm("Do you want to enter your age  again?");
+//     }
+
+
+// Problem-4
+
+// let number = prompt("Enter your Number");
+// number = Number.parseInt(number);
+
+// if(number > 4){
+//     location.href = "https://google.com"
+// }
+
+// Problem-5
+
+// let color = prompt("Enter your color");
+
+// document.body.style.background = color;
+
+
