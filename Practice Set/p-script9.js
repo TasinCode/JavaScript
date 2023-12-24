969a6e02a7d7eb3c78e5aa75863016a3b4ac6061
codewithtasin <codewithtasin@gmail.com>
2023-12-24 18:54:36 +0600

    Practice Set-9

---
+++ Practice Set/p-script9.js
@@ -0,0 +1,17 @@
+//  Problem-1
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
+
+// Problem-2
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "blue";
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green";
+
+
+// Problem-3
+
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "cyan"
+});
+
+// Problem-5
